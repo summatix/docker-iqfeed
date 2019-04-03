4d8c11ef9075c2d87e31e2e32af886c6f7e5c4dc
Robert Roose <robert@shivercube.com>
2019-04-03 21:36:36 +0200

    Using historical data for testing instead of ticks

--- tests/test_iqfeed_client.py
+++ tests/test_iqfeed_client.py
@@ -17,13 +17,15 @@ def wait_for_iqfeed():
 def test_can_pull_data_from_specific_time_period():
     results = pkgutil.get_data("tests.data", "SPY_20190311_to_20190318.txt").decode("utf-8")
 
-    hist_conn = iq.HistoryConn(name="pyiqfeed-Example-tickdata")
-    hist_listener = iq.VerboseIQFeedListener("History Tick Listener")
+    hist_conn = iq.HistoryConn(name="pyiqfeed-Example-historical-bars")
+    hist_listener = iq.VerboseIQFeedListener("History Bar Listener")
     hist_conn.add_listener(hist_listener)
 
     with iq.ConnConnector([hist_conn]) as connector:
-        data = hist_conn.request_ticks_in_period(
+        data = hist_conn.request_bars_in_period(
             ticker="SPY",
+            interval_len=14400,
+            interval_type="s",
             bgn_prd=datetime.datetime(
                 year=2019,
                 month=3,
@@ -37,8 +39,7 @@ def test_can_pull_data_from_specific_time_period():
                 day=18,
                 hour=9,
                 minute=30
-            ),
-            max_ticks=100
+            )
         )
 
         print(data)
